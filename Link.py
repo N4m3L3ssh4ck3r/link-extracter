498e7a5aba8722f277e0b3d02ba3c0ca2c077c4b
N4m3L3ssh4ck3r <113279739+N4m3L3ssh4ck3r@users.noreply.github.com>
2023-10-09 04:18:08 +0300

    Update Link.py

--- Link.py
+++ Link.py
@@ -1,15 +1,17 @@
 import threading
-import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import validators
 import time
 import random
 
-# Function to make a request and handle errors
-def connector(url):
-    result = False
-    user_agent_list = [
+# User agents list
+user_agent_list = [
         # Chrome
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
         'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
@@ -37,25 +39,38 @@ def connector(url):
         'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
     ]
 
-    user_agent = random.choice(user_agent_list)
-    headers = {'User-Agent': user_agent}
+# Function to initialize the WebDriver and visit the URL with a random user agent
+def initialize_driver(url):
+    options = Options()
+    options.add_argument("--headless")  # Run headless for performance
+    options.add_argument(f"user-agent={random.choice(user_agent_list)}")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    return driver
+
+# Function to make a request and handle errors using Selenium WebDriver
+def connector_selenium(url, driver):
     try:
-        response = requests.get(url, headers=headers, timeout=30)
-        result = response.text
-    except requests.ConnectionError as e:
-        raise ConnectionError("\u001b[31;1mCan not connect to server. Check your internet connection\u001b[0m")
-    except requests.Timeout as e:
-        raise TimeoutError("\u001b[31;1mOOPS!! Timeout Error\u001b[0m")
-    except requests.RequestException as e:
-        raise AttributeError("\u001b[31;1mError in HTTP request\u001b[0m")
-    except KeyboardInterrupt:
-        raise KeyboardInterrupt("\u001b[31;1mInterrupted by user\u001b[0m")
+        driver.get(url)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+        response = driver.page_source
+        return response
     except Exception as e:
         raise RuntimeError("\u001b[31;1m%s\u001b[0m" % (e))
-    finally:
-        if not result:
-            print("\u001b[31;1mBad Internet Connection :(\u001b[0m")
-        return result
+
+# Function to extract links from the page using Selenium
+def extract_links_selenium(url, driver, visible_links, hidden_links):
+    response_text = connector_selenium(url, driver)
+    if response_text:
+        soup = BeautifulSoup(response_text, 'html.parser')
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if href and href != "#":
+                full_url = urljoin(url, href)
+                if validators.url(full_url) and urlparse(full_url).netloc == base_domain:
+                    visible_links.add(full_url)
+                else:
+                    hidden_links.add(full_url)
 
 # Ask the user for the target website
 url = input("Please enter the URL of the website you want to extract links from: ")
@@ -63,42 +78,18 @@ url = input("Please enter the URL of the website you want to extract links from:
 # Parse the base URL to extract the domain
 base_domain = urlparse(url).netloc
 
-# Create a list of different headers to rotate through
-headers = [{'User-Agent': 'Mozilla/5.0'}, {'User-Agent': 'Chrome/70.0.3538.77'}, {'User-Agent': 'Safari/537.36'}, {'User-Agent': 'Internet Explorer/11.0'}]
+# Create sets to store all links, visible links, and hidden links
+all_links = set()
+visible_links = set()
+hidden_links = set()
 
-# Create a set to store all the unique links
-links = set()
+# Initialize the WebDriver
+driver = initialize_driver(url)
 
-# Set the number of retries and the delay between retries
-max_retries = 3
-retry_delay = 3  # seconds
-
-# Function to extract links from a given header
-def extract_links(header):
-    response_text = connector(url)
-    if response_text:
-        # Parse the response using BeautifulSoup
-        soup = BeautifulSoup(response_text, 'html.parser')
-        
-        # Find all <a> tags in the response
-        for link in soup.find_all('a'):
-            # Get the href attribute of the <a> tag
-            href = link.get('href')
-            
-            # Check if the link is valid and not empty
-            if href and href != "#":
-                # Get the full URL by joining with the base URL
-                full_url = urljoin(url, href)
-                
-                            # Check if the full URL is valid and belongs to the same domain
-                if validators.url(full_url) and urlparse(full_url).netloc == base_domain:
-                    # Append the valid full URL to the set
-                    links.add(full_url)
-
-# Create threads for each header
+# Create threads for link extraction
 threads = []
-for header in headers:
-    thread = threading.Thread(target=extract_links, args=(header,))
+for _ in range(5):  # You can adjust the number of threads as needed
+    thread = threading.Thread(target=extract_links_selenium, args=(url, driver, visible_links, hidden_links))
     threads.append(thread)
     thread.start()
     time.sleep(1)  # Add a small delay between starting threads
@@ -106,6 +97,15 @@ for header in headers:
 # Wait for all threads to complete
 for thread in threads:
     thread.join()
+
+# Combine all links into one set
+all_links.update(visible_links)
+all_links.update(hidden_links)
+
+# Print the summary
+print(f"Total links: {len(all_links)}")
+print(f"Visible links: {len(visible_links)}")
+print(f"Hidden links: {len(hidden_links)}")
 
 # Print a success message
 print("All links from the specified domain have been extracted.")
@@ -116,9 +116,11 @@ file_name = input("Please enter the file name to save the links to (include .txt
 # Open the file in write mode
 with open(file_name, 'w') as f:
     # Write each link to a new line in the file
-    for link in links:
+    for link in all_links:
         f.write(link + "\n")
 
-# Print a success message
-print("All links from the specified domain have been extracted and saved to " + file_name + ".")
+# Close the WebDriver
+driver.quit()
 
+# Print a success message
+print(f"All links from the specified domain have been extracted and saved to {file_name}.")
